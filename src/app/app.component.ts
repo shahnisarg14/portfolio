c0597537f29ea7c8850edad1cec50b07c8593c50
Nisarg Shah <shahnisarg087@gmail.com>
2020-08-02 17:15:46 -0700

    Remove vertical menu from Experience section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,5 @@ export class AppComponent implements OnInit {
   title = 'portfolio';
 
   ngOnInit(): void {
-    document.getElementById('defaultOpen').click();
-  }
-
-  openWork(event, employer) {
-    let i;
-    let menuContent;
-    let tablinks;
-    menuContent = document.getElementsByClassName('menu-content');
-    for (i = 0; i < menuContent.length; i++) {
-      menuContent[i].style.display = 'none';
-    }
-    tablinks = document.getElementsByClassName('menu-item');
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(' active', '');
-    }
-    document.getElementById(employer).style.display = 'block';
-    event.currentTarget.className += ' active';
   }
 }
